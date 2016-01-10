e9a7844ffd88e059161284b9ea2d3c70e19be878
M-Doja <micdoja@yahoo.com>
2016-01-10 11:30:36 -0800

    push

--- public/javascript/controllers/MainAngController.js
+++ public/javascript/controllers/MainAngController.js
@@ -1,6 +1,5 @@
 angular.module('AngMtr').controller('MainAngController', function($http, $state,$scope, $mdDialog, $mdMedia){
   var vm = this;
-  console.log('The Main Page');
   vm.pageClass = 'page-home';
   vm.pageClass = 'page-about';
   vm.pageClass = 'page-contact';
